c81207629d2e354238d4d082147cdda9c243b95d
Julien Duranleau <julien.duranleau@gmail.com>
2020-11-15 21:08:31 -0500

    Sound editor ui wip

--- src/sound_editor.js
+++ src/sound_editor.js
@@ -40,14 +40,15 @@ export function init() {
             y: 30,
             w: 4,       // only 2 wide drawing
             h: 64,
-            mem_addr: vm.addr.sfx + sfx_index * 68 + n * 2,
             mouse_down() {
+                const mem_addr = vm.addr.sfx + sfx_index * 68 + n * 2
                 const pitch = this.y + this.h - my() - 1
-                poke(this.mem_addr, (peek(this.mem_addr) & 0b11000000) ^ pitch)
+                poke(mem_addr, (peek(mem_addr) & 0b11000000) ^ pitch)
             },
             render() {
-                const pitch = peek(this.mem_addr) & 0b00111111
-                const volume = (peek(this.mem_addr + 1) & 0b00001110) >> 1
+                const mem_addr = vm.addr.sfx + sfx_index * 68 + n * 2
+                const pitch = peek(mem_addr) & 0b00111111
+                const volume = (peek(mem_addr + 1) & 0b00001110) >> 1
 
                 if (pitch > 0 && volume > 0) {
                     rectfill(this.x + 1, this.y + this.h - pitch, 2, pitch, 1)
@@ -63,19 +64,113 @@ export function init() {
             y: 101,
             w: 4,       // only 2 wide drawing
             h: 18,
-            mem_addr: vm.addr.sfx + sfx_index * 68 + n * 2,
             mouse_down() {
+                const mem_addr = vm.addr.sfx + sfx_index * 68 + n * 2
                 const volume = Math.max(0, Math.round((this.y + 14 - my()) / 2))
-                poke(this.mem_addr + 1, (peek(this.mem_addr + 1) & 0b11110001) ^ (volume << 1))
+                poke(mem_addr + 1, (peek(mem_addr + 1) & 0b11110001) ^ (volume << 1))
             },
             render() {
-                const volume = (peek(this.mem_addr + 1) & 0b00001110) >> 1
+                const mem_addr = vm.addr.sfx + sfx_index * 68 + n * 2
+                const volume = (peek(mem_addr + 1) & 0b00001110) >> 1
                 if (volume > 0) {
                     rectfill(this.x, this.y + this.h - (volume * 2) - 3, 3, 2, 14)
                 }
             }
         }
     })
+
+    const prev_sfx = {
+        x: 5,
+        y: 12,
+        w: 3,
+        h: 5,
+        click() {
+            sfx_index = (sfx_index === 0)
+                ? 63
+                : sfx_index - 1
+        },
+        render() {
+            color(14)
+            pset(this.x, this.y + 2)
+            line(this.x + 1, this.y + 1, this.x + 1, this.y + this.h - 2)
+            line(this.x + 2, this.y, this.x + 2, this.y + this.h - 1)
+        }
+    }
+
+    const next_sfx = {
+        x: 21,
+        y: 12,
+        w: 3,
+        h: 5,
+        click() {
+            sfx_index = (sfx_index === 63)
+                ? 0
+                : sfx_index + 1
+        },
+        render() {
+            color(14)
+            pset(this.x + 2, this.y + 2)
+            line(this.x + 1, this.y + 1, this.x + 1, this.y + this.h - 2)
+            line(this.x, this.y, this.x, this.y + this.h - 1)
+        }
+    }
+
+    const current_sfx_label = {
+        render() {
+            print(`${sfx_index}`.padStart(2, '0'), 11, 12, 7)
+        }
+    }
+
+    const pitch_label = {
+        render() {
+            print(`:PITCH`, 3, 21, 13)
+        }
+    }
+
+    const speed_label = {
+        render() {
+            print(`SPD`, 43, 12, 6)
+        }
+    }
+
+    const loop_label = {
+        render() {
+            print(`LOOP`, 83, 12, 6)
+        }
+    }
+
+    const speed = {
+        x: 56,
+        y: 11,
+        w: 12,
+        h: 7,
+        render() {
+            rectfill(this.x, this.y, this.w, this.h, 0)
+            print("09", this.x + 1, this.y + 1, 6)
+        }
+    }
+
+    const loop_start = {
+        x: 100,
+        y: 11,
+        w: 12,
+        h: 7,
+        render() {
+            rectfill(this.x, this.y, this.w, this.h, 0)
+            print("09", this.x + 1, this.y + 1, 6)
+        }
+    }
+
+    const loop_end = {
+        x: 113,
+        y: 11,
+        w: 12,
+        h: 7,
+        render() {
+            rectfill(this.x, this.y, this.w, this.h, 0)
+            print("09", this.x + 1, this.y + 1, 6)
+        }
+    }
 
     const playback = {
         playing: false,
@@ -91,4 +186,13 @@ export function init() {
     for (const note of notes) stage.push(note)
     for (const volume of volumes) stage.push(volume)
     stage.push(playback)
+    stage.push(prev_sfx)
+    stage.push(next_sfx)
+    stage.push(current_sfx_label)
+    stage.push(pitch_label)
+    stage.push(speed_label)
+    stage.push(loop_label)
+    stage.push(speed)
+    stage.push(loop_start)
+    stage.push(loop_end)
 }
